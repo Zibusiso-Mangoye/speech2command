32cad070ee71fa06470c3c3696a535322b231b33
Zibusiso <znmangoye@gmail.com>
2022-09-02 16:52:28 +0200

    Structured plugins to be directories instead of files

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,9 +1,10 @@
+from abc import ABC
 import os
 import traceback
 from importlib import util
 
 
-class Base:
+class S2CPlugin(ABC):
     """Basic resource class. Concrete resources will inherit from this one
     """
     plugins = []
@@ -13,6 +14,13 @@ class Base:
     def __init_subclass__(cls, **kwargs):
         super().__init_subclass__(**kwargs)
         cls.plugins.append(cls)
+        
+    def execute(self, command: str) -> None:
+        """Executes command provided
+        Args:
+            command (str): command to be executed
+        """
+        pass
 
 
 # Small utility to automatically load modules
@@ -28,11 +36,27 @@ def load_module(path):
 path = os.path.abspath(__file__)
 dirpath = os.path.dirname(path)
 
-for fname in os.listdir(dirpath):
-    # Load only "real modules"
-    if not fname.startswith('.') and \
-       not fname.startswith('__') and fname.endswith('.py'):
-        try:
-            load_module(os.path.join(dirpath, fname))
-        except Exception:
-            traceback.print_exc()
+# for fname in os.listdir(dirpath):
+#     # Load only "real modules"
+#     if not fname.startswith('.') and \
+#        not fname.startswith('__') and fname.endswith('.py'):
+#         try:
+#             load_module(os.path.join(dirpath, fname))
+#         except Exception:
+#             traceback.print_exc()
+
+for subdir in os.scandir(dirpath):
+    subdir_name = subdir.path.split("\\")[-1]
+    if not os.path.isdir(subdir) and subdir_name.startswith('.') or subdir_name.startswith('__'):
+        continue
+    subdir_path = os.path.join(dirpath, subdir_name)
+    for file in os.scandir(subdir_path):
+        if file.path.startswith('.') or file.path.startswith('__'):
+            continue
+        if os.path.isfile(file.path) and file.path.split("\\")[-1] == subdir_name and file.path.split("\\")[-1].endswith(".py"):
+            try:
+                filename = file.path.split("\\")[-1]
+                load_module(os.path.join(dirpath, file.path.split("\\")[-1]))
+                print(f">> loaded module {filename}")
+            except Exception:
+                traceback.print_exc()
--- plugins/test_plugin/test_plugin.py
+++ plugins/test_plugin/test_plugin.py
@@ -6,6 +6,6 @@ class PluginA(plugins.Base):
     def __init__(self):
         pass
 
-    def print(self, command):
+    def execute(self, command):
         print(">> TEST PLUGIN PRINTING")
         print(command)
